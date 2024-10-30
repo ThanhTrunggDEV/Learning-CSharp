09d0b73b663038b1c9ba7259095a419f3c6022e8
ThanhTrunggDEV <trungff07@gmail.com>
2024-10-30 14:27:52 +0700

    new change

--- Program.cs
+++ Program.cs
@@ -15,111 +15,20 @@ using System.Data.Common;
 using System.ComponentModel.Design;
 using System.Runtime.Remoting.Messaging;
 using System.ComponentModel;
-
+using ThanhTrung;
 namespace Learning_CSharp
 {
-
     internal class Program
     {
-        #region Nothing Here
-        static void randomColor()
-        {
-            Random random = new Random();
-            int check = random.Next(0, 9);
-            if (check == 0)
-                ForegroundColor = ConsoleColor.Magenta;
-            if (check == 1)
-                ForegroundColor = ConsoleColor.DarkBlue;
-            if (check == 2)
-                ForegroundColor = ConsoleColor.DarkGray;
-            if (check == 3)
-                ForegroundColor = ConsoleColor.DarkGreen;
-            if (check == 4)
-                ForegroundColor = ConsoleColor.DarkCyan;
-            if (check == 5)
-                ForegroundColor = ConsoleColor.DarkRed;
-            if (check == 6)
-                ForegroundColor = ConsoleColor.DarkMagenta;
-            if (check == 7)
-                ForegroundColor = ConsoleColor.DarkYellow;
-            if (check == 8)
-                ForegroundColor = ConsoleColor.Cyan;
-        }
-        static void Loading()
-        {
-            randomColor();
-            Write("\rProcessing.");
-            Sleep(1000);
-            randomColor();
-            Write("\rProcessing..");
-            Sleep(1000);
-            randomColor();
-            Write("\rProcessing...");
-            Sleep(500);
-            for (int i = 0; i <= 100; i++)
-            {
-                Sleep(10);
-                randomColor();
-                Write($"\rLoading {i}%...");
-                if (i == 100)
-                {
-                    Clear();
-                    Write("\rCompleted");
-                }
-            }
-            Thread.Sleep(500);
-            Clear();
-        }
-        static void Generate()
-        {
-            Console.Write("Enter a string: ");
-            string input = Console.ReadLine();
-            Loading();
-            string output = string.Empty;
-            int position = 0;
-            while (true)
-            {
-                for (char character = 'A'; character <= 'z'; character++)
-                {
-                    Sleep(50);
-                    randomColor();
-                    Write("\r\n" + output + character);
-                    Title = output;
-                    if (character == input[position])
-                    {
-                        output += input[position++];
-                    }
-                    else if (input[position] == ' ')
-                    {
-                        output += " ";
-                        position++;
-                    }
-                    if (position == input.Length)
-                    {
-                        Write("\n");
-                        return;
-                    }
-                }
-            }
-        }
-        #endregion
         static void Main(string[] args)
         {
             #region Console Configuration
             Title = "Written And Designed By ThanhTrung";
-            randomColor();
+            Advanced.randomColor();
             OutputEncoding = Encoding.UTF8;
             #endregion
-
-            string[] s = File.ReadAllLines("D:\\FaceBook Friends\\FB friends.txt");
-            
-            foreach(var item in s)
-            {
-                if (item.Contains("NNguyễn Chí KKhởi"))
-                {
-                    WriteLine(item);
-                }
-            }
+            ShoppingApp.Init();
+            ShoppingApp.Start();
         }
     }
 }
