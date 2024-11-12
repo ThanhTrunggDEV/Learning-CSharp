21beafeac63f4e1fbc7ad785dd9818e8ed7531cc
ThanhTrungDev <trungff07@gmail.com>
2024-11-12 08:19:43 +0700

    New

--- Program.cs
+++ Program.cs
@@ -30,6 +30,12 @@ namespace Learning_CSharp
 {
     internal class Program 
     {
+        static int Max(int a, int b)
+        {
+            if (a > b)
+                return a;
+            return b;
+        }
         static void Main(string[] args)
         {
             #region Console Configuration
@@ -37,7 +43,32 @@ namespace Learning_CSharp
             randomColor();
             OutputEncoding = Encoding.UTF8;
             #endregion
-            Console.WriteLine("Just Test");
+            int t = int.Parse(ReadLine());
+            while(t > 0)
+            {
+                int n = int.Parse(ReadLine());
+                var arr = new int[n];
+                int maxl = 1;
+                for(int i = 0; i < n; i++)
+                {
+                    arr[i] = int.Parse(ReadLine());
+                }
+                int maxtemp = 1;
+                for(int j = 1; j < n; j++)
+                {
+                    if (arr[j - 1] < arr[j])
+                    {
+                        maxtemp++;
+                    }
+                    else
+                    {
+                        maxl = Max(maxl,maxtemp);
+                        maxtemp = 1;
+                    }
+                }
+                t--;
+                WriteLine("\t" + maxl);
+            }
         }
     }
 }
