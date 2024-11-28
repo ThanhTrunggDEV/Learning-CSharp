36f143d3d9b2c9b4ff1fee7e78181a4c335dfcf5
ThanhTrungDev <trungff07@gmail.com>
2024-11-28 22:40:28 +0700

    New

--- Program.cs
+++ Program.cs
@@ -7,11 +7,36 @@ using System.Threading;
 using static System.Math;
 using static System.Threading.Thread;
 using static ThanhTrung.Advanced;
+using System.Globalization;
 namespace Learning_CSharp
 {
 
     internal class Program
     {
+        public static void InputRandom(int[] arr)
+        {
+            Random rd = new Random();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rd.Next(101);
+            }
+        }
+        public static void Input(int[] arr)
+        {
+            Random rd = new Random();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = int.Parse(ReadLine());
+            }
+        }
+        public static void Print(int[] arr)
+        {
+            foreach (var item in arr)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
         static void Main(string[] args)
         {
             #region Console Configuration
@@ -19,7 +44,11 @@ namespace Learning_CSharp
             randomColor();
             OutputEncoding = Encoding.UTF8;
             #endregion
-           
+            int n = int.Parse(ReadLine());
+            int[] arr = new int[n];
+            InputRandom(arr);
+            Print(arr);
+
         }
     }
 }
