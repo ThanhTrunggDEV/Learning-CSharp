9ad5f0982eb2a85edbadc3f3f02c60c84f0f7460
ThanhTrunggDEV <trungff07@gmail.com>
2024-11-30 09:56:11 +0700

    new

--- OOP_Exercise.cs
+++ OOP_Exercise.cs
@@ -1,4 +1,5 @@
-﻿using NAudio.CoreAudioApi.Interfaces;
+﻿using Learning_CSharp.Zoo_Management;
+using NAudio.CoreAudioApi.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,58 +8,133 @@ using System.Threading.Tasks;
 
 namespace Learning_CSharp
 {
-    public class Student : IComparer<Student>
+    public class Animal
     {
-        private string fullName;
+        private string name;
         private int age;
-        public string FullName { get { return fullName; } set { fullName = value; } }
+        private string species;
+        public string Name { get { return name; } set { name = value; } }
         public int Age { get { return age; } set { age = value; } }
-        public Student()
+        public string Species { get { return species; } set { species = value; } }
+        public Animal()
         {
-            FullName = string.Empty;
-            Age = 0;
+            name = string.Empty;
+            age = 0;
+            species = string.Empty;
         }
-        public Student(string FullName, int Age)
+        public Animal(string Name, int Age, string Species)
         {
+            this.Name = Name;
             this.Age = Age;
-            this.FullName = FullName;
+            this.Species = Species;
         }
-        public int Compare(Student x, Student y)
+        public virtual void MakeSound()
         {
-            if (x.FullName.CompareTo(y.FullName) == 1)
-                return 1;
-            return -1;
-
-        }
-        public override string ToString()
-        {
-            return $"Full Name: {FullName} Age: {Age}";
+            Console.WriteLine("Animal Sound!!");
         }
     }
-    public class Pro
+    public class Lion : Animal
     {
-        private int val;
-
-        public int Val { get { return val; } set { val = value; } }
-
-        public Pro() { }
-        public Pro(int val)
+        public bool IsAlpha { get; set; }
+        public Lion(): base()
         {
-            this.Val = val;
+            IsAlpha = false;
         }
-
-        public static Pro operator +(Pro a, Pro b)
+        public Lion(string Name, int Age, string Species, bool IsAlpha) : base(Name, Age, Species)
         {
-            return new Pro(a.val + b.val);
+            this.IsAlpha = IsAlpha;
         }
-
+        public override void MakeSound()
+        {
+            Console.WriteLine($"{Name} a lion is speaking");
+        }
         public override string ToString()
         {
-            return val.ToString();
+            return $"Name: {Name}\n" +
+                   $"Species: {Species}\n" +
+                   $"Age: {Age}\n" +
+                   $"Alphal: {IsAlpha}\n";
         }
-        public static Pro operator ++(Pro a)
+    }
+    public class Parrot : Animal
+    {
+        public void RepeatWord(string word)
         {
-            return new Pro(a.val+1);
+            Console.WriteLine(word);
+        }
+        public override void MakeSound()
+        {
+            Console.WriteLine($"{Name} a parrot is speaking");
+        }
+        public Parrot() : base() { }
+        public Parrot(string Name, int Age, string Species) : base(Name, Age, Species) { }
+        public override string ToString()
+        {
+            return $"Name: {Name}\n" +
+                   $"Species: {Species}\n" +
+                   $"Age: {Age}\n";
+        }
+    }
+    public class Dolphin : Animal
+    {
+        public void DoFlip()
+        {
+            Console.WriteLine($"{Name} a dolphin does a flip");
+        }
+        public Dolphin() : base() { }
+        public Dolphin(string Name, int Age, string Species) : base(Name, Age, Species) { }
+        public override void MakeSound()
+        {
+            Console.WriteLine($"{Name} a Dolphine is speaking");
+        }
+        public override string ToString()
+        {
+            return $"Name: {Name}\n" +
+                   $"Species: {Species}\n" +
+                   $"Age: {Age}\n";
+        }
+    }
+    public class Zoo
+    {
+        private List<Animal> animalList;
+        public List<Animal> AnimalList { get { return animalList; } set { animalList = value; } }
+        public Zoo()
+        {
+            animalList = new List<Animal>();
+        }
+        public Zoo(List<Animal> animalList)
+        {
+            AnimalList = animalList;
+        }
+        public void AddAnimal(Animal animal)
+        {
+            animalList.Add(animal);
+        }
+        public void DisplayAllAnimals()
+        {
+            foreach(Animal animal in AnimalList)
+            {
+                Console.WriteLine(animal);
+            }
+        }
+        public void HearAnimalSounds()
+        {
+            foreach (Animal animal in AnimalList)
+            {
+                animal.MakeSound();
+            }
+        }
+        public void AnimalAction()
+        {
+            foreach (var item in animalList)
+            {
+                if (item is Lion lion)
+                    continue;
+                if (item is Parrot parrot)
+                    parrot.RepeatWord("Hello");
+                if (item is Dolphin dolphin)
+                    dolphin.DoFlip();
+            }
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -13,19 +13,6 @@ namespace Learning_CSharp
 
     internal class Program
     {
-        struct Student
-        {
-            public int Id;
-            public string Name;
-            public float GPA;
-            public Student(int id, string name, float gpa)
-            {
-                Id = id;
-                Name = name;
-                GPA = gpa;
-            }
-            
-        }
         static void Main(string[] args)
         {
             #region Console Configuration
@@ -33,28 +20,16 @@ namespace Learning_CSharp
             randomColor();
             OutputEncoding = Encoding.UTF8;
             #endregion
-            List<Student> students = new List<Student>();
-            students.Add(new Student(1, "Thanh Trung", 3.9f));
-            students.Add(new Student(3, "Byan", 3.8f));
-            students.Add(new Student(2, "Cack", 3.7f));
-            students.Add(new Student(4, "Dra", 3.6f));
-            students.Add(new Student(5, "Anh", 3.5f));
-            for(int i = 0; i < students.Count; i++)
-            {
-                int minIndex = i;
-                for(int j = i + 1; j < students.Count; j++)
-                {
-                    if (students[minIndex].Name[0] > students[j].Name[0])
-                        minIndex = j;
-                }
-                Student temp = students[minIndex];
-                students[minIndex] = students[i];
-                students[i] = temp;
-            }
-            foreach (Student student in students)
-            {
-                Console.WriteLine($" ID: {student.Id} Name: {student.Name} GPA: {student.GPA}");
-            }
+            Zoo zoo = new Zoo();
+            Animal parrot = new Parrot("Jack",20, "Bird");
+            Animal lion = new Lion("Simba", 10, "Carnivores", false);
+            Animal dolphin = new Dolphin("Leo", 30, "Fish");
+            zoo.AddAnimal(parrot);
+            zoo.AddAnimal(lion);
+            zoo.AddAnimal(dolphin);
+            zoo.DisplayAllAnimals();
+            zoo.HearAnimalSounds();
+            zoo.AnimalAction();
         }
     }
 }
