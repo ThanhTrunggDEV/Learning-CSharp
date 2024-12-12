6b3e0bae583298dc41c0028e447fa8a21676612e
ThanhTrunggDEV <trungff07@gmail.com>
2024-12-12 12:38:06 +0700

    new OOP ex

---
+++ Hospital Management System/System.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning_CSharp.Hospital_Management_System
+{
+    internal class System
+    {
+    }
+}
