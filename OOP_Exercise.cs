430be3eb248c5a97f77ff46b4ac85895aac80ebc
Nguyễn Thành Trung <trungff07@gmail.com>
2025-02-09 19:25:59 +0700

    nothing

--- OOP_Exercise.cs
+++ OOP_Exercise.cs
@@ -8,8 +8,127 @@ using System.Diagnostics.Contracts;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media;
 
 namespace Learning_CSharp
 {
-    
+    public interface ISwim
+    {
+         void Swim();
+    }
+    public abstract class Animal
+    {
+        private string name;
+        private int age;
+
+        public string Name { get { return name; } set { name = value; } }
+        public int Age { get { return age; } set { age = value; } }
+        public Animal(string name = "", int age = 0)
+        {
+            Name = name;
+            Age = age;
+        }
+        public virtual void Make_Sound()
+        {
+            Console.WriteLine("An animal is making sound!!!");
+        }
+    }
+    public class Lion : Animal, ISwim
+    {
+        private string maneColor;
+        public string ManeColor { get { return maneColor; } set { maneColor = value; } }
+        
+        public Lion(string ManeColor = "", string Name = "", int Age = 0) : base(Name, Age)
+        {
+            this.ManeColor = ManeColor;
+        }
+        public override void Make_Sound()
+        {
+            Console.WriteLine($"My name is {Name}, I'm a lion");
+        }
+        public override string ToString()
+        {
+            return $"Name: {Name} | Age: {Age} | Mane Color: {ManeColor}";
+        }
+        public void Swim()
+        {
+            Console.WriteLine($"{Name} a lion, is swimming");
+        }
+    }
+    public class Dog : Animal, ISwim
+    {
+        private string color;
+        public string Color { get { return color; } set { color = value; } }
+        public Dog(string Color = "", string Name = "", int Age = 0) : base(Name, Age)
+        {
+            this.Color = Color;
+        }
+        public override void Make_Sound()
+        {
+            Console.WriteLine($"My name is {Name}, I'm a dog");
+        }
+        public override string ToString()
+        {
+            return $"Name: {Name} | Age: {Age} | Color: {Color}";
+        }
+        public void Swim()
+        {
+            Console.WriteLine($"{Name} a dog, is swimming");
+        }
+    }
+
+    public class Cat : Animal
+    {
+        private string furColor;
+        public string FurColor { get { return furColor; } set { furColor = value; } }
+        public Cat(string FurColor = "", string Name = "", int Age = 0) : base(Name, Age)
+        {
+            this.FurColor = FurColor;
+        }
+        public override void Make_Sound()
+        {
+            Console.WriteLine($"My name is {Name}, I'm a cat");
+        }
+        public override string ToString()
+        {
+            return $"Name: {Name} | Age: {Age} | Fur Color: {FurColor}";
+        }
+    }
+    public class Zoo
+    {
+        private List<Animal> animals;
+        public List<Animal> Animals { get { return animals; } set { animals = value; } }
+        public Zoo()
+        {
+            Animals = new List<Animal>();
+        }
+        public void AddAnimal(Animal animal)
+        {
+            Animals.Add(animal);
+        }
+        public void DisPlay()
+        {
+            if(Animals.Count <= 0) Console.WriteLine("There is no animals in the zoo");
+            foreach (var animal in animals)
+            {
+                Console.WriteLine(animal);
+            }
+        }
+        public void Perform()
+        {
+            Console.WriteLine("Perform Making Sound: ");
+            foreach (var animal in animals)
+            {
+                animal.Make_Sound();
+            }
+            Console.WriteLine("Perform Swimming: ");
+            foreach (var animal in animals)
+            {
+                if (animal is ISwim swimmer)
+                {
+                    swimmer.Swim();
+                }
+            }
+        }
+    }
 }
--- Program.cs
+++ Program.cs
@@ -17,137 +17,10 @@ using OfficeOpenXml.LoadFunctions.Params;
 using System.Runtime.CompilerServices;
 namespace Learning_CSharp
 {
-    public class Node : IComparer<Node>
-    {
-        public int val;
-        public Node next = null;
-        public Node(int x)
-        {
-            val = x;
-        }
-        public Node() { }
-        public int Compare(Node x, Node y)
-        {
-            if(x.val < y.val) return -1;
-
-            return 1;
-        }
-    }
-    
-    public class LinkedList
-    {
-        public Node head;
-        public int length;
-        public LinkedList()
-        {
-            head = null;
-            length = 0;
-        }
-    }
-
            
     internal class Program
     {
-        static void Push_back(int val, LinkedList list)
-        {
-            Node newNode = new Node(val);
-            list.length++;
-            Node temp = list.head;
-            if (list.head == null)
-            {
-               list.head = newNode;
-                return;
-            }
-            while (temp.next != null)
-            {
-                temp = temp.next;
-            }
-            temp.next = newNode;
-        }
-        static void Pop_back(LinkedList list)
-        {
-            list.length--;
-            Node temp = list.head;
-            for(int i = 0; i < list.length - 2; i++)
-            {
-                temp = temp.next;
-            }
-            temp.next = null;
-        }
-        static void Remove(LinkedList list, int index)
-        {
-            
-            Node prevNode = list.head;
-            Node temp = prevNode.next;
-            if (index < 0 || index >= list.length) return;
-            list.length--;
-            if(index == 0)
-            {
-                list.head = list.head.next;
-                return;
-            }
-            for (int i = 1;i < list.length; i++)
-            {
-                if (i == index)
-                {
-                    prevNode.next = temp.next;
-                    return;
-                }
-                prevNode = temp;
-                temp = temp.next;
-            }
-
-        }
-        static void Insert(LinkedList list, int index, int val)
-        {
-            if (index < 0 || index >= list.length) return;
-            Node newNode = new Node(val);
-            list.length++;
-            if(index == 0)
-            {
-                Node temp = list.head;
-                list.head = newNode;
-                newNode.next = temp;
-                return;
-            }
-            Node node = list.head.next;
-            Node prevNode = list.head;
-            for(int i = 1; i < list.length; i++)
-            {
-                if(i == index)
-                {
-                    prevNode.next = newNode;
-                    newNode.next = node;
-                    return;
-                }
-                prevNode = node;
-                node = node.next;
-            }
-        }
-        static Node Reverse(LinkedList list)
-        {
-           Node prevNode = null;
-           Node curNode = list.head;
-            Node nextNode = curNode.next;
-            while(curNode != null)
-            {
-                nextNode = curNode.next;
-                curNode.next = prevNode;
-                prevNode = curNode;
-                curNode = nextNode;
-                
-            }
-            return prevNode;
-        }
-        static void Print(LinkedList list)
-        {
-            Node temp = list.head;
-            while (temp != null)
-            {
-                WriteLine(temp.val);
-                temp = temp.next;
-            }
-        }
+        
         static void Main(string[] args)
         {
             #region Console Configuration
@@ -155,15 +28,21 @@ namespace Learning_CSharp
             randomColor();
             OutputEncoding = Encoding.UTF8;
             #endregion
-            string s = "[";
-            Random rd = new Random();
-            for(int i = 0; i < 200; i++)
-            {
-                s+= rd.Next(0, 10).ToString();
-                s += ",";
-            }
-            
-            Console.WriteLine(s);
+            Zoo zoo = new Zoo();
+            zoo.AddAnimal(new Lion("Yellow", "Simba", 5));
+            zoo.AddAnimal(new Dog("Black", "Husky", 3));
+            zoo.AddAnimal(new Lion("Red", "Mufasa", 10));
+            zoo.AddAnimal(new Dog("White", "Akita", 2));
+            zoo.AddAnimal(new Lion("Brown", "Scar", 7));
+            zoo.AddAnimal(new Dog("Brown", "Pug", 1));
+            zoo.AddAnimal(new Lion("Black", "Nala", 6));
+            zoo.AddAnimal(new Dog("Yellow", "Golden Retriever", 4));
+            zoo.AddAnimal(new Lion("White", "Sarabi", 8));
+            zoo.AddAnimal(new Dog("Red", "Dalmatian", 5));
+            zoo.AddAnimal(new Lion("Yellow", "Simba", 5));
+            zoo.AddAnimal(new Dog("Black", "Husky", 3));
+            zoo.DisPlay();
+            zoo.Perform();
         }
     }
 }
